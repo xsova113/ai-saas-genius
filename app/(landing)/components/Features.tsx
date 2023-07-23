3af503bab8b00e4c262b0e9a689265cdd14c0c3d
George <xsova113@gmail.com>
2023-07-23 01:20:27 -0700

    minor update

--- app/(landing)/components/Features.tsx
+++ app/(landing)/components/Features.tsx
@@ -9,22 +9,52 @@ const Features = () => {
           <div className="chat chat-end">
             <div className="chat-bubble">
               <h1 className="chat-header">Conversation Generator</h1>
-              <p className="chat-footer">haha</p>
+              <p className="">
+                Start a conversation with Genius and ask whatever you want!
+              </p>
             </div>
           </div>
-          <span className="">haha</span>
+          <div className="chat chat-end">
+            <div className="chat-bubble">
+              <h1 className="chat-header">Image Generator</h1>
+              <p className="">
+                Start a conversation with Genius and ask whatever you want!
+              </p>
+            </div>
+          </div>
         </div>
 
         <div className="flex flex-col items-center col-span-6">
           <div className="relative w-[700px] h-96">
             <Image src={"/empty.png"} alt={"image"} fill />
           </div>
-          <div>haha</div>
+          <div className="card bg-neutral">
+            <div className="card-body items-center ">
+              <h1 className="card-title">Conversation Generator</h1>
+              <p className="">
+                Start a conversation with Genius and ask whatever you want!
+              </p>
+            </div>
+          </div>
         </div>
 
         <div className="flex flex-col col-span-3 h-64 justify-between">
-          <span>hahahadsfasdfa</span>
-          <span>hahahahha</span>
+          <div className="chat chat-start">
+            <div className="chat-bubble">
+              <h1 className="chat-header">Conversation Generator</h1>
+              <p className="">
+                Start a conversation with Genius and ask whatever you want!
+              </p>
+            </div>
+          </div>
+          <div className="chat chat-start">
+            <div className="chat-bubble">
+              <h1 className="chat-header">Conversation Generator</h1>
+              <p className="">
+                Start a conversation with Genius and ask whatever you want!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
