61010a6da7405e9fc21b3532d3db14536debbb10
George <xsova113@gmail.com>
2023-07-23 00:30:03 -0700

    minor update

--- app/(landing)/components/Features.tsx
+++ app/(landing)/components/Features.tsx
@@ -6,7 +6,12 @@ const Features = () => {
     <div className="px-4 md:px-10 my-8 md:my-20 pb-10 text-slate-200">
       <div className="grid grid-cols-12">
         <div className="flex flex-col col-span-3 h-64 justify-between">
-          <span>haha</span>
+          <div className="chat chat-end">
+            <div className="chat-bubble">
+              <h1 className="chat-header">Conversation Generator</h1>
+              <p className="chat-footer">haha</p>
+            </div>
+          </div>
           <span className="">haha</span>
         </div>
 
