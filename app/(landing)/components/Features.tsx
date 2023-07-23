ec66c1d7995e682f91ed673e88373ce275c068ee
George <xsova113@gmail.com>
2023-07-23 00:05:04 -0700

    minor update

--- app/(landing)/components/Features.tsx
+++ app/(landing)/components/Features.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const Features = () => {
   return (
-    <div className="px-4 md:px-10 pb-10 text-slate-200">
+    <div className="px-4 md:px-10 my-8 md:my-20 pb-10 text-slate-200">
       <div className="grid grid-cols-12">
-        <div className="flex flex-col col-span-3">
+        <div className="flex flex-col col-span-3 h-64 justify-between">
           <span>haha</span>
           <span className="">haha</span>
         </div>
@@ -17,7 +17,7 @@ const Features = () => {
           <div>haha</div>
         </div>
 
-        <div className="flex flex-col col-span-3">
+        <div className="flex flex-col col-span-3 h-64 justify-between">
           <span>hahahadsfasdfa</span>
           <span>hahahahha</span>
         </div>
