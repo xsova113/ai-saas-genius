d58c4a59159ab43fda1baa5860748009bf78daa2
George <xsova113@gmail.com>
2023-07-26 00:59:50 -0700

    minor update

--- app/(landing)/components/showCase.tsx
+++ app/(landing)/components/showCase.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { Parallax } from "react-scroll-parallax";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useRef } from "react";
+import { cn } from "@/lib/utils";
 
 const images = ["/conversation.png", "/image.png", "/video.png", "/code.png"];
 
@@ -25,7 +26,7 @@ const Showcase = () => {
   return (
     <Parallax
       speed={20}
-      className="flex flex-col md:px-14 px-8 bg-gradient-to-r from-blue-400 to-emerald-500 mt-24 md:mt-32 items-center"
+      className="flex flex-col md:px-14 px-8 bg-gradient-to-r from-blue-800 to-emerald-800 mt-24 md:mt-32 items-center"
     >
       <div
         ref={element}
@@ -48,13 +49,13 @@ const Showcase = () => {
         ))}
       </div>
       <button
-        className="btn btn-circle absolute top-1/3 right-5 opacity-80"
+        className={cn("btn btn-circle absolute top-1/3 right-5 opacity-80")}
         onClick={onRightClick}
       >
         <ChevronRight />
       </button>
       <button
-        className="btn btn-circle absolute top-1/3 left-5 opacity-80"
+        className={cn("btn btn-circle absolute top-1/3 left-5 opacity-80")}
         onClick={onLeftClick}
       >
         <ChevronLeft />
